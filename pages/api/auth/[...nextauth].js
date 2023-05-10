617ff6cce09c0b8401bca99e79495022824d4edd
Guilherme <devguicordeiro@gmail.com>
2023-05-10 19:18:33 -0300

    added connection to mongodb and bfixes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from "@/lib/mongodb"
 
 export default NextAuth({
   providers: [
@@ -7,5 +9,6 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     }),
-  ]
+  ],
+  adapter: MongoDBAdapter(clientPromise),
 })
