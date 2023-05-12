67babfb2e9157a530020ace47d511d3d2810c87b
Guilherme <devguicordeiro@gmail.com>
2023-05-11 21:21:19 -0300

    quick update, new pages

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ import Layout from "@/components/Layout"
 import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
+  const {data:session} = useSession();
   return (
   <Layout>
-    Página de controle
-    <button onClick={() => signOut()}>Sign out</button>
+    <div className="text-purple-900">
+      Olá, <b>{session?.user?.name}</b>
+    </div>
   </Layout>
   )
 }
--- pages/products.js
+++ pages/products.js
@@ -1,9 +1,10 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function Products() {
     return(
         <Layout>
-            Página de proutos
+            <Link className="bg-gray-400 py-1 px-2 rounded-md" href={"/products/new"}>Adicionar novo produto</Link>
         </Layout>
     )
 
