6676c958f8e32168df9fae4134bef7d2a8d65891
Guilherme <devguicordeiro@gmail.com>
2023-05-12 13:01:45 -0300

    added axios and mongoose to create and store product

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,9 +1,42 @@
 import Layout from "@/components/Layout";
+import { useState } from "react";
 
 export default function NewProduct() {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPrice] = useState("");
+    async function createProduct(ev) {
+        ev.preventDefault();
+        const data = {title, description, price}
+        await axios.post("/api/products", data);
+    }
+
     return(
         <Layout>
-            Novo Produto
+        <form onSubmit={createProduct}>
+            <h1 >Novo Produto</h1>
+            <label>Nome do produto</label>
+            <input 
+                type="text" 
+                placeholder="Insira o nome aqui" 
+                value={title}
+                onChange={ev => setTitle(ev.target.value)} >
+            </input>
+            <label>Descrição do produto</label>
+            <textarea 
+                placeholder ="Insira a descrição aqui"
+                value={description}
+                onChange={ev => setDescription(ev.target.value)} >
+            </textarea>
+            <label>Preço do produto</label>
+            <input 
+                type="number" 
+                placeholder="Insira o preço aqui"
+                value={price}
+                onChange={ev => setPrice(ev.target.value)} >
+            </input>
+            <button type="submit" className="create-new-btn">Criar</button>
+        </form>
         </Layout>
     )
 }
