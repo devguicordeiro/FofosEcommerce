ad2fc1d0a2dbb2e131ce071b2928d763814d85fd
Guilherme <devguicordeiro@gmail.com>
2023-05-15 12:23:37 -0300

    added edit product

--- pages/api/products.js
+++ pages/api/products.js
@@ -4,6 +4,15 @@ import {mongooseConnect} from "@/lib/mongoose";
  export default async function handle(req, res) {
     const {method} = req;
     await mongooseConnect();
+
+    if(method === "GET") {
+        if(req.query?.id) {
+            res.json(await Product.findOne({_id:req.query.id}))
+        } else {
+            res.json(await Product.find());
+        }
+    }
+
     if (method === "POST"){
         const {title, description, price} = req.body;
         const productDoc = await Product.create({
