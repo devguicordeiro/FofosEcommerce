77a69be9aa8ce5708bf9b000a88f535dedf128ae
Guilherme <devguicordeiro@gmail.com>
2023-05-31 19:51:48 -0300

    added sub categorys, buttons and functionality half way through

--- models/Category.js
+++ models/Category.js
@@ -2,7 +2,7 @@ import mongoose, {Schema, model, models} from "mongoose";
 
 const CategorySchema = new Schema({
     name: {type: String, required:true},
-    parent: {type:mongoose.Types.ObjectId},
+    parent: {type:mongoose.Types.ObjectId, ref: "Category"},
 });
 
 export const Category = models?.Category || model("Category", CategorySchema);
