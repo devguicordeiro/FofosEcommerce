db213eafd1ef180c8126d4ca581d45cdae51791d
Guilherme <devguicordeiro@gmail.com>
2023-05-31 18:27:59 -0300

    bug solved, moving on

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -2,19 +2,30 @@ import { mongooseConnect } from "@/lib/mongoose";
 import { Category } from "@/models/Category";
 
 export default async function handle(req, res) {
-    const {method} = req;
-    await mongooseConnect();
+  const { method } = req;
+  await mongooseConnect();
 
-    if (method === "GET"){
-        res.json(await Category.find());
-    }
+  if (method === "GET") {
+    res.json(await Category.find());
+  }
 
-    if (method === "POST") {
-        const {name, parentCategory} = req.body;
-        const categoryDoc = await Category.create({
-            name, 
-            parent:parentCategory
-        });
-        res.json(categoryDoc);
+  if (method === "POST") {
+    const { name, parentCategory } = req.body;
+
+    try {
+      let parent = null;
+      if (parentCategory !== "") {
+        parent = parentCategory;
+      }
+
+      const categoryDoc = await Category.create({
+        name,
+        parent,
+      });
+      res.json(categoryDoc);
+    } catch (error) {
+      console.error("Error creating category:", error);
+      res.status(400).json({ error: "Invalid parent category ID" });
     }
+  }
 }
--- pages/categories.js
+++ pages/categories.js
@@ -3,57 +3,74 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Categories() {
-    const[name,setName] = useState("");
-    const[categories, setCategories] = useState([]);
-    const[parentCategory, setParentCategory] = useState("");
-    useEffect(() => {
-        fetchCategories();
-    }, []);
-    function fetchCategories() {
-        axios.get("/api/categories").then(result => {
-            setCategories(result.data);
-        });
-    };
-    async function saveCategory(ev){
-        ev.preventDefault();
-        axios.post("/api/categories", {name, parentCategory});
-        setName("");
-        fetchCategories();
-    }
-    return(
-        <Layout>
-            <h1>Categories Page</h1>
-            <label>Nova Categoria</label>
-            <form onSubmit={saveCategory} className="flex gap-1">
-                <input  className="m-0"
-                        value={name} 
-                        onChange={ev => setName(ev.target.value)} 
-                        type="text" placeholder={"Nome da Categoria"}>
-                </input>
-                <select className="m-0" 
-                        onChange={ev => setParentCategory(ev.target.value)}
-                        value={parentCategory}>
-                    <option value="">Categoria Principal</option>
-                    {categories.length > 0 && categories.map(category => (
-                        <option value={category._id}>{category.name}</option>
-                    ))}
-                </select>
-                <button type="submit" className="bg-purple-700 text-white border border-black px-2 rounded-md">Salvar</button>
-            </form>
-            <table className="basict mt-4">
-                <thead>
-                    <tr>
-                        <td>Nome da Categoria</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {categories.length > 0 && categories.map(category => (
-                        <tr>
-                            <td>{category.name}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </Layout>
-    )
+  const [name, setName] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [parentCategory, setParentCategory] = useState("");
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  function fetchCategories() {
+    axios.get("/api/categories").then((result) => {
+      setCategories(result.data);
+    });
+  }
+
+  async function saveCategory(ev) {
+    ev.preventDefault();
+    axios.post("/api/categories", { name, parentCategory });
+    setName("");
+    fetchCategories();
+  }
+
+  return (
+    <Layout>
+      <h1>Categories Page</h1>
+      <label>Nova Categoria</label>
+      <form onSubmit={saveCategory} className="flex gap-1">
+        <input
+          className="m-0"
+          value={name}
+          onChange={(ev) => setName(ev.target.value)}
+          type="text"
+          placeholder={"Nome da Categoria"}
+        />
+        <select
+          className="m-0"
+          onChange={(ev) => setParentCategory(ev.target.value)}
+          value={parentCategory}
+        >
+          <option value="">Categoria Principal</option>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <option key={category._id} value={category._id}>
+                {category.name}
+              </option>
+            ))}
+        </select>
+        <button
+          type="submit"
+          className="bg-purple-700 text-white border border-black px-2 rounded-md"
+        >
+          Salvar
+        </button>
+      </form>
+      <table className="basict mt-4">
+        <thead>
+          <tr>
+            <td>Nome da Categoria</td>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <tr key={category._id}>
+                <td>{category.name}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
 }
