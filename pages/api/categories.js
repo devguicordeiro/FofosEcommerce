9a8f455eaccae285933cafa24bf971b48e93b50d
Guilherme <devguicordeiro@gmail.com>
2023-06-01 09:01:52 -0300

    pause while stuck in a bug with sweetalert

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -28,4 +28,30 @@ export default async function handle(req, res) {
       res.status(400).json({ error: "Invalid parent category ID" });
     }
   }
+
+  if (method === "PUT") {
+        const { name, parentCategory, _id } = req.body;
+
+    try {
+      let parent = null;
+      if (parentCategory !== "") {
+        parent = parentCategory;
+      }
+
+      const categoryDoc = await Category.updateOne({_id}, {
+        name,
+        parent,
+      });
+      res.json(categoryDoc);
+    } catch (error) {
+      console.error("Error creating category:", error);
+      res.status(400).json({ error: "Invalid parent category ID" });
+    }
+  }
+
+  if(method === "DELETE") {
+    const {_id} = req.query;
+    await Category.deleteOne({_id});
+    res.json("ok");
+  }
 }
--- pages/categories.js
+++ pages/categories.js
@@ -1,8 +1,9 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { withSwal } from 'react-sweetalert2';
 
-export default function Categories() {
+function Categories({swal}) {
   const[editedCategory, setEditedCategory] = useState(null);
   const [name, setName] = useState("");
   const [categories, setCategories] = useState([]);
@@ -20,14 +21,41 @@ export default function Categories() {
 
   async function saveCategory(ev) {
     ev.preventDefault();
-    axios.post("/api/categories", { name, parentCategory });
+    const data = {name, parentCategory}
+    if(editedCategory) {
+      data._id = editedCategory._id;
+      await axios.put("/api/categories", data);
+      setEditedCategory(null);
+    } else {
+      await axios.post("/api/categories", data);
+    }
     setName("");
     fetchCategories();
   }
 
   function editCategory(category){
     setEditedCategory(category);
+    setName(category.name);
+    setParentCategory(category.parent?._id);
   }
+
+  function deleteCategory(category) {
+    swal.fire({
+      title: 'Você tem certeza?',
+      text: `Você realmente deseja deletar a categoria ${category.name}?`,
+      showCancelButton: true,
+      cancelButtonText: "Cancelar",
+      confirmButtonText: "Sim, Deletar!",
+      confirmButtonColor: "#991b1b",
+      cancelButtonColor: "#9ca3af",
+  }).then(async result => {
+      if(result.isConfimed) {
+        const {_id} = category;
+        await axios.delete("/api/categories?_id"+_id);
+        fetchCategories();
+      }
+  });
+  };
 
   return (
     <Layout>
@@ -84,7 +112,9 @@ export default function Categories() {
                                 </svg>
                                 <span>Editar</span>
                         </button>
-                        <button className="create-new-btn flex items-center text-sm">
+                        <button 
+                          onClick={() => deleteCategory(category)}
+                          className="create-new-btn flex items-center text-sm">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                             </svg>
@@ -99,3 +129,7 @@ export default function Categories() {
     </Layout>
   );
 }
+
+export default withSwal(({swal}, ref) => (
+  <Categories swal={swal}></Categories>
+))
