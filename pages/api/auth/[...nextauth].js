ee35c741a969a9cf5637ca8f4acd31ba23a0c2f7
Guilherme <devguicordeiro@gmail.com>
2023-06-02 22:15:54 -0300

    security part 1

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,11 @@
-import NextAuth from 'next-auth'
+import NextAuth, { getServerSession } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
 import clientPromise from "@/lib/mongodb"
 
-export default NextAuth({
+const adminEmails = ["devguicordeiro@gmail.com"];
+
+export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
@@ -11,4 +13,23 @@ export default NextAuth({
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
-})
+  callbacks: {
+    session: ({session,account,profile}) => {
+      if (adminEmails.includes(session?.user?.email)){
+        return session;
+      } else {
+        return false;
+      }
+
+    }
+  }
+}
+
+export default NextAuth(authOptions);
+
+export async function isAdminRequest(req, res) {
+  const session = await getServerSession(req, res, authOptions);
+  if (!adminEmails.includes(session?.user?.email)) {
+    throw "you're not an admin"
+  }
+}
