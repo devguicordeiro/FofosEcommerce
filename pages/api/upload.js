5cbf08de05f73c1d2c1025ccec55000fcaba5211
Guilherme <devguicordeiro@gmail.com>
2023-06-25 15:51:26 -0300

    for some unkown reason some imports went missing

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -2,6 +2,8 @@ import multiparty from "multiparty";
 import {PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
 import fs from "fs";
 import mime from "mime-types";
+import { mongooseConnect } from "@/lib/mongoose";
+import { isAdminRequest } from "./auth/[...nextauth]";
 const bucketName = "fofos-ecommerce"
 
 export default async function handle(req, res) {
