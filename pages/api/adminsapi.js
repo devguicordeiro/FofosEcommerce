aa6a3c17164261b5d951401983e839b7dcde5d43
Guilherme <devguicordeiro@gmail.com>
2023-07-13 14:41:54 -0300

    working delete btn adm

--- pages/api/adminsapi.js
+++ pages/api/adminsapi.js
@@ -9,6 +9,11 @@ export default async function handle(req, res) {
         const {email} = req.body;
         res.json(await Admin.create({email}))
     }
+    if (req.method === "DELETE") {
+        const {_id} = req.query;
+        await Admin.findByIdAndDelete(_id);
+        res.json(true);
+    }
     if (req.method === "GET") {
         res.json (await Admin.find());
     }
