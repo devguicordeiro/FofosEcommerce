3a8d3f343e071bac52fbbfd1ad2afef141d4b04e
Guilherme <devguicordeiro@gmail.com>
2023-07-14 13:56:07 -0300

    working change on select of featured

--- pages/settings.js
+++ pages/settings.js
@@ -1,14 +1,38 @@
 import Layout from "@/components/Layout";
+import Spinner from "@/components/Spinner";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function SettingsPage() {
+    const[products,  setProducts] = useState([]);
+    const[featuredId, setFeaturedId] = useState(null);
+    const[isLoading, setIsLoading] = useState(false);
+    useEffect(() => {
+        setIsLoading(true);
+        axios.get("/api/products").then(res => {
+            setProducts(res.data);
+            setIsLoading(false);
+        })
+    }, [])
     return(
         <Layout>
             <h1>Configurações do Site</h1>
-            <label>Produto em Destaque</label>
-            <select></select>
-            <div>
-                <button className="btn-default">Salvar</button>
-            </div>
+            {isLoading && (
+                <Spinner />
+            )}
+            {!isLoading && (
+                <>
+                    <label>Produto em Destaque</label>
+                    <select onChange={ev => setFeaturedId(ev.target.value)} >
+                        {products.length > 0 && products.map(product => (
+                            <option key={product._id} value={product._id}>{product.title}</option>
+                        ))}
+                    </select>
+                    <div>
+                        <button className="btn-default">Salvar</button>
+                    </div>
+                </>
+            )}
         </Layout>
     )
 
